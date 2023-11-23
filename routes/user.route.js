0d5110e88f9eb47d677a8a09424bc9cfdcfaa8b1
Omobolanle Afolabi <afolabiomobolanle00@gmail.com>
2023-11-23 09:55:05 +0100

    get

--- routes/user.route.js
+++ routes/user.route.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.get('/api/v1/user',userCtrl.getUser);
 router.post('/api/v1/user',userCtrl.addUser);
-router.get('/api/v1/user/:id',userCtrl.getUserById)
+router.get('/api/v1/user/:id',userCtrl.getUserById);
+router.get('/api/v1/user/login',userCtrl.loginUser);
 
 module.exports = router;
