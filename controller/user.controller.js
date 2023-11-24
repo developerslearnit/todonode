5241e354d58ecd9893244460d705af46e46e779e
Omobolanle Afolabi <afolabiomobolanle00@gmail.com>
2023-11-24 08:03:43 +0100

    commit

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,4 +1,4 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 var db = new PrismaClient();
 
 const getUser = async (req, res) => {
@@ -6,58 +6,49 @@ const getUser = async (req, res) => {
   res.status(200).json(user);
 };
 
-
-const getUserById = async(req,res) =>{
+const getUserById = async (req, res) => {
   const user = await db.user.findUnique({
-      where: {
-        id: req.params.id
-      }
-   
+    where: {
+      id: req.params.id,
+    },
   });
-  res.status(200).json(user)
-}
+  res.status(200).json(user);
+};
 
 const addUser = async (req, res) => {
   try {
-    const {userName,firstName ,lastName, email, password } = req.body;
- 
+    const { userName, firstName, lastName, email, password } = req.body;
+
     const newUser = await db.user.create({
-        data: {
-          userName:userName,
-          firstName: firstName,
-          lastName: lastName,
-          email: email,
-          password: password,
-        },
-      });
-      res.status(200).json(newUser);
+      data: {
+        userName: userName,
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password,
+      },
+    });
+    res.status(200).json(newUser);
   } catch (error) {
     res.status(500).json({
-      error:error.message
-    })
+      error: error.message,
+    });
   }
-
 };
 
-const loginUser = async(req, res)=>{
-  const {email, password} = req.body;
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
 
   const existingUser = await db.user.findFirst({
     where: {
-      email:email,
-      password:password
+      email: email,
+      password: password,
+    },
+  });
+  if (!existingUser) {
+    return res.status(404).json({ message: "User does not exist" });
   }
-}
-  )
-  if (!existingUser){
-    return res.status(404).json({message:'User does not exist'})
+  res.status(200).json(existingUser);
+};
 
-  }
-  res.status(200).json(existingUser)
-
-}
-
-
-
-
-module.exports = { getUser, addUser, getUserById, loginUser};
+module.exports = { getUser, addUser, getUserById, loginUser };
